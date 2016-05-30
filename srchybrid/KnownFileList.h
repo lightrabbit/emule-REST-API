074958a9747a357e2888de7c452acda5532d5356
Yuzu <NickLiu635@outlook.com>
2016-05-30 22:41:58 +0800

    柚子未解决的编译错误，在WebServerRESTAPI.cpp的第555行

--- srchybrid/KnownFileList.h
+++ srchybrid/KnownFileList.h
@@ -60,6 +60,18 @@ public:
 	CKnownFile* FindKnownFile(LPCTSTR filename, uint32 date, uint64 size) const;
 	CKnownFile* FindKnownFileByID(const uchar* hash) const;
 	CKnownFile* FindKnownFileByPath(const CString& sFilePath) const;
+	POSITION usedToFindByNumber;
+	void CKnownFileList::FindHeadKnownFile() { usedToFindByNumber=m_Files_map.GetStartPosition(); }
+	CKnownFile* FindNextKnownFile() { 
+		CKnownFile* cur_file;
+		CCKey key;
+		if (usedToFindByNumber) {
+			m_Files_map.GetNextAssoc(usedToFindByNumber, key, cur_file);
+			return cur_file;
+		}else return NULL; 
+	}
+
+
 	bool	IsKnownFile(const CKnownFile* file) const;
 	bool	IsFilePtrInList(const CKnownFile* file) const;
 
