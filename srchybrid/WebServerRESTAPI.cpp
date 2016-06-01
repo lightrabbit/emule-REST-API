22e37534239f751d7cfa7f9b2e6d8b54c248d577
Yuzu <NickLiu635@outlook.com>
2016-06-01 20:18:08 +0800

    删除了绝大多数并没有什么卵用的属性。

--- srchybrid/WebServerRESTAPI.cpp
+++ srchybrid/WebServerRESTAPI.cpp
@@ -100,49 +100,41 @@ public:
 	//我忽然想到 WriteObiect的函数名或许有歧义，
 	//可能与前端操作对象（比如增加好友）的函数名重复？@ 2016-5-24 3：00
 	//CFriend类的风格与CServer类的风格有一定差异，需要注意一下。
-	void Object(CClientCredits* credit, unsigned char index = 0, CUpDownClient* client = NULL) {
+	void Object(CClientCredits* credit, unsigned char index = 0) {
 		if (!index)StartObject();
 		Key(_T("uploadedTotal")); Uint64(credit->GetUploadedTotal());
 		Key(_T("downloadedTotal")); Uint64(credit->GetDownloadedTotal());
-		if (client) {
-			bool ans = credit->GetHasScore(client);
-			Key(_T("hasScore")); Bool(ans);
-			if (ans) {
-				Key(_T("currentIdentState")); Uint(credit->GetCurrentIdentState(client->GetIP()));
-				/*
-				enum EIdentState{
-					IS_NOTAVAILABLE,
-					IS_IDNEEDED,
-					IS_IDENTIFIED,
-					IS_IDFAILED,
-					IS_IDBADGUY,
-				};
-				*/
-			}
-		}
 		if (!index)EndObject();
 	}
 	//TODO:完成CUpDownClient类型的返回
 	//list=CClientList
+	CString ArrToHex(const uchar* str, unsigned int len) {
+		const char T16[] = "0123456789abcdef";
+		char hex[4095];
+		if (len != 0) {
+			for (int i = 0; i < len; i++) {
+				hex[i * 2] = T16[str[i] % 16];
+				hex[i * 2 + 1] = T16[str[i] / 16];
+			}
+			hex[i * 2] = '\0';
+			return CString(hex);
+		}
+		return CString("");
+	}
 	void Object(CUpDownClient* client, unsigned char index = 0)
 	{ //我注意到CUpDownClient类中有GetFriend()方法返回CFriend类 
 	  //CFriend类中有GetLinkedClient()方法返回CUpDownClient类
 	  //如何才能避免无限递归？
 	  //一个可行的方法是，所有Object中调用的Object方法，只返回索引信息，不返回详细信息
-		//if (index)
-		{
-			Key(_T("clientUserHash")); String(CString(client->GetUserHash()));//uchar[16]
-		}
-		//else
-		{
-			StartObject();
-			Key(_T("uploadDatarate")); Uint(client->GetUploadDatarate());
-			Key(_T("userHash")); String(CString(client->GetUserHash()));
-			Key(_T("clientVersion")); Int(client->GetClientSoft());
-			Key(_T("isBanned")); Bool(client->IsBanned());
-			Key(_T("userName")); String(CString(client->GetUserName()));
-			Key(_T("SoftVer")); String(client->GetClientSoftVer());
-			/*
+
+		StartObject();
+		Key(_T("uploadDatarate")); Uint(client->GetUploadDatarate());
+		Key(_T("userHash")); String(CString(ArrToHex(client->GetUserHash(),16)));
+		Key(_T("hashType")); Int(client->GetHashType());
+		Key(_T("isBanned")); Bool(client->IsBanned());
+		Key(_T("userName")); String(CString(client->GetUserName()));
+		Key(_T("SoftVer")); String(client->GetClientSoftVer());
+		/*
 			case SO_EMULE:			return _T("1");
 			case SO_OLDEMULE:		return _T("1");
 			case SO_EDONKEY:		return _T("0");
@@ -153,168 +145,35 @@ public:
 			case SO_LPHANT:			return _T("l");
 			case SO_URL:			return _T("u");
 			*/
-			Key(_T("IP")); Int(client->GetIP());//
-			Key(_T("isFriend")); Bool(client->IsFriend());
-			//Object(client->CheckAndGetReqUpFile());//CKnownFile
-			Key(_T("transferredUp")); Int(client->GetTransferredUp());
-			Key(_T("transferredDown")); Int(client->GetTransferredDown());
-			Key(_T("clientModVer")); String(client->GetClientModVer());
-			Key(_T("version")); Int(client->GetVersion());
-			Key(_T("muleVersion")); Int(client->GetMuleVersion());
-
-			if (client->Credits())Object(client->Credits(), index + 1, client);	//TODO: !!!Object for CClientCredits
-			else {
-				Key(_T("credit")); String(_T("null"));
-			}
-
-			/*//先全部注释掉吧
-			Key(_T("Client")); String(_T("NotSupporttedYet"));
-			Key(_T("isEd2kClient")); Bool(client->IsEd2kClient());
-			Key(_T("checkHandshakeFinished")); Bool(client->CheckHandshakeFinished());
-			Key(_T("userIDHybrid")); Int(client->GetUserIDHybrid());
-
-			Key(_T("hasLowID")); Bool(client->HasLowID());
-			Key(_T("connectIP")); Int(client->GetConnectIP());
-			Key(_T("userPort")); Int(client->GetUserPort());
-
-			Key(_T("serverIP")); Int(client->GetServerIP());
-			Key(_T("serverPort")); Int(client->GetServerPort());
-			Key(_T("userHash")); String(CString(client->GetUserHash()));
-			Key(_T("hasValidHash")); Bool(client->HasValidHash());
-			Key(_T("hashType")); Int(client->GetHashType());
-			Key(_T("buddyID")); String(CString(client->GetBuddyID()));
-			Key(_T("hasValidBuddyID")); Bool(client->HasValidBuddyID());
-			Key(_T("buddyIP")); Int(client->GetBuddyIP());
-			Key(_T("buddyPort")); Int(client->GetBuddyPort());
-			//Object(writer,client->GetClientSoft());	//TODO: !!!Object for EClientSoftware
-
-			Key(_T("isExtProtocolAvailable")); Bool(client->ExtProtocolAvailable());
-			Key(_T("isSupportMultiPacket")); Bool(client->SupportMultiPacket());
-			Key(_T("isSupportExtMultiPacket")); Bool(client->SupportExtMultiPacket());
-			Key(_T("isSupportPeerCache")); Bool(client->SupportPeerCache());
-			Key(_T("isSupportsLargeFiles")); Bool(client->SupportsLargeFiles());
-			Key(_T("isSupportsFileIdentifiers")); Bool(client->SupportsFileIdentifiers());
-			Key(_T("isEmuleClient")); Bool(client->IsEmuleClient());
-			Key(_T("sourceExchange1Version")); Int(client->GetSourceExchange1Version());
-			Key(_T("isSupportsSourceExchange2")); Bool(client->SupportsSourceExchange2());
-
-			Key(_T("clientFilename")); String(client->GetClientFilename());
-			Key(_T("uDPPort")); Int(client->GetUDPPort());
-			Key(_T("uDPVersion")); Int(client->GetUDPVersion());
-			Key(_T("isSupportsUDP")); Bool(client->SupportsUDP());
-			Key(_T("kadPort")); Int(client->GetKadPort());
-			Key(_T("extendedRequestsVersion")); Int(client->GetExtendedRequestsVersion());
-			//Object(client->GetConnectingState());//TODO: !!!WriterObject for EConnectingState
-			Key(_T("lastSrcReqTime")); Int(client->GetLastSrcReqTime());
-			Key(_T("lastSrcAnswerTime")); Int(client->GetLastSrcAnswerTime());
-			Key(_T("lastAskedForSources")); Int(client->GetLastAskedForSources());
-			Key(_T("friendSlot")); Bool(client->GetFriendSlot());
-			Key(_T("isFriend")); Bool(client->IsFriend());
-
-			//Object(client->GetFriend(),index+1);//警告：可能产生无穷递归
-
-			Key(_T("sentCancelTransfer")); Bool(client->GetSentCancelTransfer());
-			Key(_T("kadVersion")); Int(client->GetKadVersion());
-			Key(_T("isSendBuddyPingPong")); Bool(client->SendBuddyPingPong());
-			Key(_T("isAllowIncomeingBuddyPingPong")); Bool(client->AllowIncomeingBuddyPingPong());
-			Key(_T("secureIdentState")); Int(client->GetSecureIdentState());
-			Key(_T("infoPacketsReceived")); Bool(client->GetInfoPacketsReceived());
-			Key(_T("isSupportPreview")); Bool(client->GetPreviewSupport());
-			Key(_T("isSupportViewSharedFiles")); Bool(client->GetViewSharedFilesSupport());
-			Key(_T("isRequestsCryptLayer")); Bool(client->RequestsCryptLayer());
-			Key(_T("isRequiresCryptLayer")); Bool(client->RequiresCryptLayer());
-			Key(_T("isSupportsDirectUDPCallback")); Bool(client->SupportsDirectUDPCallback());
-			Key(_T("isObfuscatedConnectionEstablished")); Bool(client->IsObfuscatedConnectionEstablished());
-			Key(_T("isShouldReceiveCryptUDPPackets")); Bool(client->ShouldReceiveCryptUDPPackets());
-			WriterObject(writer, client->GetUploadState());	//TODO: WriterObject for EUploadState
-			Key(_T("waitStartTime")); Int64(client->GetWaitStartTime());
-			Key(_T("waitTime")); Int(client->GetWaitTime());
-			Key(_T("isDownloading")); Bool(client->IsDownloading());
-			Key(_T("hasBlocks")); Bool(client->HasBlocks());
-			Key(_T("numberOfRequestedBlocksInQueue")); Uint(client->GetNumberOfRequestedBlocksInQueue());
-			Key(_T("upStartTimeDelay")); Uint(client->GetUpStartTimeDelay());
-			Key(_T("uploadFileID")); String(CString(client->GetUploadFileID()));
-			Key(_T("sendBlockData")); Uint(client->SendBlockData());
-			Key(_T("askedCount")); Uint(client->GetAskedCount());
-			Key(_T("lastUpRequest")); Uint(client->GetLastUpRequest());
-			Key(_T("hasCollectionUploadSlot")); Bool(client->HasCollectionUploadSlot());
-			Key(_T("sessionUp")); Uint(client->GetSessionUp());
-			Key(_T("sessionPayloadDown")); Uint(client->GetSessionPayloadDown());
-			Key(_T("queueSessionPayloadUp")); Bool(client->GetQueueSessionPayloadUp());
-			Key(_T("payloadInBuffer")); Bool(client->GetPayloadInBuffer());
-			Key(_T("upPartCount")); Uint(client->GetUpPartCount());
-			Key(_T("upPartStatus")); Uint(*(client->GetUpPartStatus()));
-			Key(_T("combinedFilePrioAndCredit")); Double(client->GetCombinedFilePrioAndCredit());
-			Key(_T("askedCountDown")); Int(client->GetAskedCountDown());
-			//Object(writer,client->GetDownloadState());	//TODO: WriterObject for EDownloadState
-			Key(_T("partStatus")); Uint(*(client->GetPartStatus()));
-			Key(_T("partCount")); Uint(client->GetPartCount());
-			Key(_T("remoteQueueRank")); Int(client->GetRemoteQueueRank());
-			Key(_T("isRemoteQueueFull")); Bool(client->IsRemoteQueueFull());
-			Key(_T("availablePartCount")); Int(client->GetAvailablePartCount());
-			Key(_T("uDPPacketPending")); Int(client->UDPPacketPending());
-			Key(_T("isSourceRequestAllowed")); Int(client->IsSourceRequestAllowed());
-			Key(_T("isValidSource")); Int(client->IsValidSource());
-			//WriterObject(client->GetSourceFrom());	//TODO: WriterObject for ESourceFrom
-			Key(_T("upCompleteSourcesCount")); Int(client->GetUpCompleteSourcesCount());
-			//WriterObject(writer,client->GetChatState());	//TODO:WriterObject for EChatState
-			//WriterObject(writer,client->GetChatCaptchaState());	//TODO:WriterObject for EChatCaptchaState
-			Key(_T("messagesReceived")); Int(client->GetMessagesReceived());
-			Key(_T("messagesSent")); Int(client->GetMessagesSent());
-			Key(_T("isSpammer")); Bool(client->IsSpammer());
-			Key(_T("messageFiltered")); Bool(client->GetMessageFiltered());
-			//WriterObject(writer,client->GetKadState());	//TODO: WriterObject for EKadState
-			Key(_T("hasFileComment")); Bool(client->HasFileComment());
-			Key(_T("fileComment")); String(client->GetFileComment());
-			Key(_T("hasFileRating")); Bool(client->HasFileRating());
-			Key(_T("reqFileAICHHash")); Object(client->GetReqFileAICHHash());
-			Key(_T("isSupportingAICH")); Bool(client->IsSupportingAICH());
-			Key(_T("isAICHReqPending")); Bool(client->IsAICHReqPending());
-			//WriterObject(writer,client->GetUnicodeSupport());	//TODO: WriterObject for EUtf8Str
-			Key(_T("downloadStateDisplayString")); String(client->GetDownloadStateDisplayString());
-			Key(_T("uploadStateDisplayString")); String(client->GetUploadStateDisplayString());
-			//WriterObject(writer,client->credits);	//TODO: WriterObject for CClientCredits
-			//WriterObject(writer,clienr->m_OtherRequests_list);
-			//WriterObject(writer,clienr->m_OtherNoNeeded_list);	//TODO: WriterObject for CTypedPtrList<CPtrList, CPartFile*>
-			Key(_T("lastPartAsked")); Int(client->m_lastPartAsked);
-			Key(_T("addNextConnect")); Bool(client->m_bAddNextConnect);
-			Key(_T("isDownloadingFromPeerCache")); Bool(client->IsDownloadingFromPeerCache());
-			Key(_T("isUploadingToPeerCache")); Bool(client->IsUploadingToPeerCache());
-			Key(_T("hasPeerCacheState")); Bool(client->HasPeerCacheState());
-			Key(_T("httpSendState")); Int(client->GetHttpSendState());
-			Key(_T("sendPeerCacheFileRequest")); Bool(client->SendPeerCacheFileRequest());
-			Key(_T("onPeerCacheDownSocketTimeout")); Bool(client->OnPeerCacheDownSocketTimeout());
-			Key(_T("upendsoon")); Bool(client->upendsoon);
-			Key(_T("isDifferentPartBlock")); Bool(client->IsDifferentPartBlock());
-			Key(_T("remainingBlocksToDownload")); Bool(client->GetRemainingBlocksToDownload());
-			Key(_T("")); Bool(client->());
-			//*/
-			EndObject();
+		Key(_T("IP")); Uint(client->GetIP());
+		Key(_T("connectIP")); Uint(client->GetConnectIP());
+		Key(_T("userPort")); Uint(client->GetUserPort());
+		Key(_T("isFriend")); Bool(client->IsFriend());
+		//Object(client->CheckAndGetReqUpFile());//CKnownFile
+		Key(_T("transferredUp")); Uint(client->GetTransferredUp());
+		Key(_T("transferredDown")); Uint(client->GetTransferredDown());
+		Key(_T("clientVersion")); Int(client->GetClientSoft());
+		Key(_T("clientModVer")); String(client->GetClientModVer());
+		Key(_T("version")); Uint(client->GetVersion());
+		Key(_T("muleVersion")); Uint(client->GetMuleVersion());
+		if (client->Credits())
+			Object(client->Credits(), index + 1);	
+		else {
+			Key(_T("credit")); String(_T("null"));
 		}
-
-	}
-
-	void Object(CFriend* pail)//因为friend是关键字，这里用pail 
-	{
-		CUpDownClient* Client;
-		Client = pail->GetLinkedClient();
-
-		StartObject();
-		Key(_T("userHash")); String(CString(pail->m_abyUserhash));//uchar[16]
-		Key(_T("lastSeen")); Int64(pail->m_dwLastSeen);//有可能是64位时间
-		Key(_T("lastUsedIP")); Int(pail->m_dwLastUsedIP);
-		Key(_T("lastUsedPort")); Int(pail->m_nLastUsedPort);
-		Key(_T("lastChatted")); Int(pail->m_dwLastChatted);
-		Key(_T("name")); String(pail->m_strName);
-		//TODO:讨论GetLinkedClient();的发送格式并实现	--准备进行
-		Object(Client);//TODO: Object for CUpDownClient
-		//以及GetClientForChatSession();的功能及实现
-		Key(_T("isTryToConnet")); Bool(pail->IsTryingToConnect());
-		Key(_T("isFriendSlotted")); Bool(pail->GetFriendSlot());
-		Key(_T("hasUserHash")); Bool(pail->HasUserhash());
-		Key(_T("hasKadID")); Bool(pail->HasKadID());
-
+		Key(_T("hasLowID")); Bool(client->HasLowID());
+		Key(_T("isEd2kClient")); Bool(client->IsEd2kClient());
+		//Object(client->GetFriend(),index+1);//警告：可能产生无穷递归
 		EndObject();
+	
+	}
+	void Object(CFriend* pail, unsigned char index = 0)//因为friend是关键字，这里用pail 
+	{
+		if (!index)StartObject();
+		Key(_T("lastSeen")); Uint64(pail->m_dwLastSeen);//有可能是64位时间
+		Key(_T("friendName")); String(pail->m_strName);
+		//Key(_T("isFriendSlotted")); Bool(pail->GetFriendSlot());
+		if (!index)EndObject();
 	}
 	//似乎返回CFriend是完全没有必要的？返回CUpDownClient类就好了
 	//里面也有isFriend方法，如果需要返回所有Friend，通过查询所有
@@ -326,95 +185,35 @@ public:
 		Key(_T("fileName")); String(file->GetFileName());
 		Key(_T("fileType")); String(file->GetFileType());
 		Key(_T("fileTypeDisplayName")); String(file->GetFileTypeDisplayStr());
-		//Object(writer,file->GetFileIdentifier());	//TODO: Object for CFileIdentifier
-		Key(_T("fileHash")); String(CString(file->GetFileHash()));
-		Key(_T("hasHashSet")); Bool(file->HasNullHash());
+		Key(_T("hasNullHash")); Bool(file->HasNullHash());
+		Key(_T("fileHash")); String(ArrToHex(file->GetFileHash(),16));
 		Key(_T("eD2kLink")); String(file->GetED2kLink());//without Hashset;HTMLTag;HostName;Source;dwSourceIP
-		//Object(writer,file->GetFileSize());	//TODO: Object for EMFileSize
-		Key(_T("isLargeFile")); Bool(file->IsLargeFile());
-		Key(_T("isPartfile")); Bool(file->IsPartFile());
+		Key(_T("fileSize")); String(CastItoXBytes(file->GetFileSize(), false, false));//TODO: Object for EMFileSize
 		Key(_T("hasComment")); Bool(file->HasComment());
-		Key(_T("userRating")); Int(file->UserRating());
 		Key(_T("hasUserRating")); Bool(file->HasRating());
+		Key(_T("userRating")); Int(file->UserRating());
 		Key(_T("hasBadRating")); Bool(file->HasBadRating());
 		Key(_T("fileComment")); String(file->GetFileComment());
 		Key(_T("fileRating")); Int(file->GetFileRating());
 		//Object(file->getNotes());	//TODO:Object for CKadEntryPtrList
-		Key(_T("isKadCommentSearchRunning")); Bool(file->IsKadCommentSearchRunning());
-		Key(_T("isCompressible")); Bool(file->IsCompressible());
 		if (!index)EndObject();
 	}
 	void Object(CShareableFile* file, unsigned char index = 0) {
 		if (!index)StartObject();
 		Object((CAbstractFile*)file, index + 1);
-		//Object(writer,file->GetVerifiedFileType());	//TODO:Object for EFileType
-		Key(_T("isPartfile")); Bool(file->IsPartFile());
 		Key(_T("path")); String(file->GetPath());
-		Key(_T("sharedDirectory")); String(file->GetSharedDirectory());
-		Key(_T("isShellLinked")); Bool(file->IsShellLinked());
 		Key(_T("filePath")); String(file->GetFilePath());
-		Key(_T("infoSummary")); String(file->GetInfoSummary());
 		if (!index)EndObject();
 	}
 	void Object(CKnownFile* file, unsigned char index = 0) {
 		if (!index)StartObject();
 		Object((CShareableFile*)file, index + 1);
-		//Object(writer,file->GetUtcCFileDate);
 		Key(_T("utcFileDate")); Int(file->GetUtcFileDate());
-		Key(_T("isShouldPartiallyPurgeFile")); Bool(file->ShouldPartiallyPurgeFile());
-		Key(_T("partCount")); Int(file->GetPartCount());
-		Key(_T("eD2KPartCount")); Int(file->GetED2KPartCount());
-		Key(_T("upPriority")); Int(file->GetUpPriority());
-		Key(_T("upPriorityEx")); Int(file->GetUpPriorityEx());
-		Key(_T("isAutoUpPriority")); Bool(file->IsAutoUpPriority());
-		Key(_T("calculateUploadPriorityPercent")); Double(file->CalculateUploadPriorityPercent());
-		Key(_T("wantedUpload")); Int64(file->GetWantedUpload());
-		Key(_T("pushfaktor")); Double(file->pushfaktor);
-		Key(_T("virtualUploadSources")); Int(file->m_nVirtualUploadSources);
-		Key(_T("virtualSourceIndicator")); Int(file->GetVirtualSourceIndicator());
-		Key(_T("virtualCompleteSourcesCount")); Int(file->m_nVirtualCompleteSourcesCount);
-		Key(_T("getOnUploadqueue")); Int(file->GetOnUploadqueue());
-		Key(_T("isPublishedED2K")); Bool(file->GetPublishedED2K());
-		Key(_T("kadFileSearchID")); Int(file->GetKadFileSearchID());
-		//Object(writer,file->GetKadKeywords())	//TODO: WriteObjects for Kademlia::WordList
-		Key(_T("lastPublishTimeKadSrc")); Int(file->GetLastPublishTimeKadSrc());
-		Key(_T("lastPublishBuddy")); Int(file->GetLastPublishBuddy());
-		Key(_T("lastPublishTimeKadNotes")); Int(file->GetLastPublishTimeKadNotes());
-		Key(_T("hasPublishSrc")); Bool(file->PublishSrc());
-		Key(_T("hasPublishNotes")); Int(file->PublishNotes());
-		Key(_T("metaDataVer")); Int(file->GetMetaDataVer());
 		Key(_T("isMovie")); Bool(file->IsMovie());
-		Key(_T("infoSummary")); String(file->GetInfoSummary());
 		Key(_T("upPriorityDisplayString")); String(file->GetUpPriorityDisplayString());
-		Key(_T("isAICHRecoverHashSetAvailable")); Bool(file->IsAICHRecoverHashSetAvailable());
 		Key(_T("utcLastModified")); Int(file->m_tUtcLastModified);
-		//Object(writer,file->statistic)	//TODO: WriteObjects for CStatisticFile
 		Key(_T("completeSourcesTime")); Int(file->m_nCompleteSourcesTime);
-		Key(_T("completeSourcesCount")); Int(file->m_nCompleteSourcesCount);
-		Key(_T("completeSourcesCountLo")); Int(file->m_nCompleteSourcesCountLo);
-		Key(_T("completeSourcesCountHi")); Int(file->m_nCompleteSourcesCountHi);
 		//Object(writer,file->m_ClientUploadList)//TODO: WriterObject for CUpDownClientPtrList
-		//Object(writer,file->m_AvailPartFrequency)//TODO: Object for CArray<uint16, uint16> 
-		//Object(writer,file->m_pCollection)//TODO: Object for CCollection*
-		Key(_T("isSR13_ImportParts")); Bool(file->SR13_ImportParts());
-		Key(_T("startUploadTime")); Int(file->GetStartUploadTime());
-		Key(_T("fileRatio")); Double(file->GetFileRatio());
-		Key(_T("isPushSmallFile")); Bool(file->IsPushSmallFile());
-		Key(_T("feedback")); String(file->GetFeedback());
-		//Object(writer,file->m_PartSentCount)//TODO: Object for CArray<uint64> 
-		Key(_T("hideOS")); Int(file->GetHideOS());
-		Key(_T("selectiveChunk")); Int(file->GetSelectiveChunk());
-		Key(_T("hideOSInWork")); Int(file->HideOSInWork());
-		Key(_T("shareOnlyTheNeed")); Int(file->GetShareOnlyTheNeed());
-		Key(_T("sotnInWork")); Int(file->SotnInWork());
-		Key(_T("powerSharedMode")); Int(file->GetPowerSharedMode());
-		Key(_T("ispowerShareAuthorized")); Bool(file->GetPowerShareAuthorized());
-		Key(_T("ispowerShareAuto")); Bool(file->GetPowerShareAuto());
-		Key(_T("powerShareLimit")); Int(file->GetPowerShareLimit());
-		Key(_T("ispowerShareLimited")); Bool(file->GetPowerShareLimited());
-		Key(_T("ispowerShared")); Bool(file->GetPowerShared());
-		Key(_T("knownStyle")); Int(file->GetKnownStyle());
-		Key(_T("psAmountLimit")); Int(file->GetPsAmountLimit());
 		if (!index)EndObject();
 	}
 	void Object(CAICHHash* hash, unsigned char index = 0) {
@@ -441,7 +240,6 @@ public:
 		if (!index)EndObject();
 	}
 };
-
 //以上
 
 CString WebServerRESTAPI::_GetServerList(ThreadData Data, CString& param)
@@ -495,7 +293,6 @@ CString WebServerRESTAPI::_GetSharedList(ThreadData Data, CString& param)
 WebServerRESTAPI::WebServerRESTAPI()
 {
 }
-
 
 WebServerRESTAPI::~WebServerRESTAPI()
 {
